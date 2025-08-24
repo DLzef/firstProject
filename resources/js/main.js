02b5319a5ba597596dbb7f8b83ebc28857ab58e1
DLzef <zefnix12345@gmail.com>
2025-08-24 21:56:48 +0200

    Added js function to count days until birthday, Added About me Section Added css for the above + some other visual changes

---
+++ resources/js/main.js
@@ -0,0 +1,10 @@
+function daysLeft() {
+    const today = new Date();
+    const target = new Date('2026-07-05');
+    const timeDifference = target - today;
+    const dayDifference = Math.ceil(timeDifference / (1000* 60 * 60 *24));
+
+document.getElementById('birthday').textContent = `Tage übrig bis zu meinem Geburtstag am 5. Juli: ${dayDifference} Tage !`;
+}
+
+daysLeft();
